dd8ebee0cb065c458fd23d3e6ccc5b0e574269fc
kavunap <pkavuna@gmail.com>
2019-09-11 20:01:15 +0000

    rpec test for task list and task creation

---
+++ spec/features/task.spec.rb
@@ -0,0 +1,37 @@
+# In this require, the feature required for Feature Spec such as Capybara are available.
+require 'rails_helper'
+
+# On the right side of this RSpec.feature, write the test item name like "task management feature" (grouped by do ~ end)
+RSpec.feature "Task management function", type: :feature do
+  # In scenario (alias of it), write the processing of the test for each item you want to check.
+  scenario "Test task list" do
+    Task.create!(name: 'test_task_01', content: 'testtesttest')
+    Task.create!(name: 'test_task_02', content: 'samplesample')
+    visit tasks_path
+    save_and_open_page
+    expect(page).to have_content 'testtesttest'
+    expect(page).to have_content 'samplesample'
+  end
+
+  scenario "Test task creation" do
+    # visit to new_task_path (transition to task registration page)
+    visit new_task_path
+
+    # In the input field labeled "Task Name" and in the input field labeled "Task Details"
+    # Fill in the task title and content respectively
+
+    fill_in  'Name' ,  with: 'completed' 
+    fill_in  'Content' ,  with: 'ruby task' 
+
+    # Click_on a button with a value (notation letter) of “Register”
+    click_on  'Create'
+
+    # Check if the information that is supposed to be registered by click is displayed on the task detail page
+    # (Assumption that transition to the task details screen will be made if the task is registered)
+    expect(page ).to  have_content  'ruby task'
+  end
+
+  scenario "Test task details" do
+
+  end
+end
