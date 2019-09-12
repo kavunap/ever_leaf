15c73a54150265c02f7300fc25241c6c1adfbf8e
kavunap <pkavuna@gmail.com>
2019-09-12 10:07:49 +0000

    locale file create

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,12 @@ module EverLeaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: true,
@@ -25,9 +25,10 @@ module EverLeaf
                        controller_specs: false,
                        request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
+      config.i18n.default_locale = :en
     end
   end
 
   
-
+  
 end
